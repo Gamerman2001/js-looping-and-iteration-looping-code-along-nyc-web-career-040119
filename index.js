60685c763a2761a7d1b9362d4bdd67076c95eea8
Gamerman2001 <Tro12185@gmail.com>
2019-05-14 20:13:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array) {
+    let shop = []
+        for (let i = 0; i < array.length; ++i) {
+            shop.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+        }
+    return shop
+}
+
+function countdown(number) {
+    let countdown = 0;
+    while (number >= countdown) {
+        console.log(number)
+        number--
+    }
+}
